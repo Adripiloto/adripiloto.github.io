0f835a309a6af99773e02a5865673b0ee5eb9a8f
Adripiloto <130670183+Adripiloto@users.noreply.github.com>
2025-03-12 19:59:50 +0100

    script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     // Realizar apuesta
        betBtn.addEventListener("click", async () => {
         try {
-            const response = await fetch("http://localhost:3000/initiate-payment", {
+            const response = await fetch("http://192.168.1.90:3000/initiate-payment", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ amount: 0.1, memo: "Slot machine bet" })
