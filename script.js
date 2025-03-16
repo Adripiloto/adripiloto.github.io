3632c2f125df97c41becc0e47acc14ff788fc65f
Adripiloto <130670183+Adripiloto@users.noreply.github.com>
2025-03-16 18:38:46 +0100

    script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
         const amount = 0.1;
         const memo = "Slot machine bet";
-        const response = await fetch("http://127.0.0.1:5000/payment/approve", {
+        const response = await fetch("http://127.0.0.1:5000/initiate-payment", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ amount, memo }),
