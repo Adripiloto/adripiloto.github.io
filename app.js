4e0ea06ef40e53355122fd47fe94248117fcfc8c
Adripiloto <130670183+Adripiloto@users.noreply.github.com>
2025-03-17 19:43:28 +0100

    app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.getElementById("auth-button").addEventListener("click", function () {
     Pi.init({
         version: "2.0",
         sandbox: true // Cambiar a false cuando pases a producción
+        appId: "sxfmtqkdbp2hp5v8rmyismfc4brgjbmbjtxakzeitelrlnvkdng04gieebb70e3u" // Reemplaza con el ID de tu app en Pi Network
     });
 
     // Autenticación del usuario
